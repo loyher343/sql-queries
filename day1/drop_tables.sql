6df4cfae935066e99e74f01474dc3b10099dd9fb
Brian Hudson <brianjasonhudson@gmail.com>
2017-03-14 11:21:22 -0500

    Adding if exists

--- day1/drop_tables.sql
+++ day1/drop_tables.sql
@@ -1,11 +1,11 @@
-drop table PlaylistTrack;
-drop table Playlist;
-drop table InvoiceLine
-drop table Invoice;
-drop table Track;
-drop table Genre;
-drop table MediaType;
-drop table Album;
-drop table Artist;
-drop table Customer;
-drop table Employee;
+drop table if exists PlaylistTrack;
+drop table if exists Playlist;
+drop table if exists InvoiceLine;
+drop table if exists Invoice;
+drop table if exists Track;
+drop table if exists Genre;
+drop table if exists MediaType;
+drop table if exists Album;
+drop table if exists Artist;
+drop table if exists Customer;
+drop table if exists Employee;
