9dd5577f0c38948523d9fa65ebfecc8d497ccd60
Brian Hudson <brianjasonhudson@gmail.com>
2017-03-14 16:56:27 -0500

    Added update, delete, joins practice

---
+++ day2/day2-practice.sql
@@ -0,0 +1,43 @@
+/* Return to https://chinook.ml. Feel free to drop the existing tables, as you will use them in the afternoon project. */
+
+--UPDATE
+    -- Create a table or use one of the sql files to create a table. 
+    -- Update one value in one row. 
+
+    -- Update multiple values in one row. 
+
+    -- Update multiple values in multiple rows.
+
+    -- Update values in rows using a subquery
+        -- For example, set all country populations to 1000000 where the populations are larger than the population of South Africa
+
+--DELETE
+    -- Delete a row from a table.
+
+    -- Delete all rows from the table.
+
+    -- Undo the operation. (Just kidding--you can't undo a delete!)
+
+--JOINS
+    -- Now we're going to need related tables. Copy the statements from world.sql into chinook in order to get started.  
+
+    -- You should see three tables. These tables share one value, the country code. We can use this code to join the tables and provide combined data. 
+
+    -- Begin by joining the country table to the countrylanguage table. 
+
+    -- Now join the city table to the country table. 
+
+    -- Finally, join all three (I'd just include one column from each) so you can see every language for every city in every country. (Note: this isn't exactly accurate. The country's langauges may not be spoken in every city.)
+
+--CALCULATIONS, AGGREGATIONS, and JOINS
+    -- Okay, now that you have the mechanics down, let's see what we can find. 
+
+    -- Find the most populous capital cities. 
+
+    -- Find the languages which have the most number of speakers in any one nation. 
+
+    -- Find the languages with the most number of speakers globally. 
+
+    -- List the countries with total_urban_population alongside their total population. For an extra challenge, try ranking the countries by the percentage of their population that is urban or rural.
+
+    
