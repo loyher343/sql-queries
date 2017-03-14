4028531f91806f23c33bf77d0315e839925c1893
Brian Hudson <brianjasonhudson@gmail.com>
2017-03-14 16:26:11 -0500

    Restructuring again to separate data for each day

---
+++ day2/sample-data/drop_tables.sql
@@ -0,0 +1,11 @@
+drop table if exists PlaylistTrack;
+drop table if exists Playlist;
+drop table if exists InvoiceLine;
+drop table if exists Invoice;
+drop table if exists Track;
+drop table if exists Genre;
+drop table if exists MediaType;
+drop table if exists Album;
+drop table if exists Artist;
+drop table if exists Customer;
+drop table if exists Employee;
